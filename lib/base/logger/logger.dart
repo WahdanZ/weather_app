857da97961afd08bd55a6ae82e2caae6cf74d8af
WahdanZ <ahmed.wahdan@outlook.com>
2024-07-16 16:27:28 +0200

    add my base mason template

--- lib/base/logger/logger.dart
+++ lib/base/logger/logger.dart
@@ -7,6 +7,7 @@ final logger = Logger(
     lineLength: 120, // width of the output
     colors: true, // Colorful log messages
     printEmojis: true, // Print an emoji for each log message
-    printTime: false, // Should each log print contain a timestamp
+    dateTimeFormat: DateTimeFormat
+        .onlyTimeAndSinceStart, // Should each log print contain a timestamp
   ),
 );
